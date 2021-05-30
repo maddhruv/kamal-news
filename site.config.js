4bccb184b26b0a936e8733b27bfc3f2009426294
Dhruv Jain <dhruvjainpenny@gmail.com>
2021-05-30 18:36:25 +0530

    feat: notch

--- site.config.js
+++ site.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  name: 'Notch',
+  name: 'Kamal News',
   notion: {
-    databaseId: 'cd5ae361ca31492282b0914928c82e7e',
+    databaseId: '9c525feae67c4f03bed590ea3c34cb7b',
   },
-  siteURL: process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'https://notch-docs.vercel.app',
+  siteURL: process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'https://kamal-news.vercel.app/',
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,9 +10,9 @@ module.exports = {
     },
     colors: {
       primary: {
-        light: "#2ecc71",
-        regular: "#27ae60",
-        dark: "#0B9A49",
+        light: "#2563EB",
+        regular: "#1D4ED8",
+        dark: "#1E40AF",
       },
       white: {
         light: "#ffffff",
